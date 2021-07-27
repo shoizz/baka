3eef6e18f953f3d1082f6bb3ff9debfdb9623c5a
shoizz <80241798+shoizz@users.noreply.github.com>
2021-07-28 06:43:38 +1000

    Update library.lua

--- library.lua
+++ library.lua
@@ -1,11 +1,19 @@
 local library = {}
 
-local function CreateInstance(cls,props)
+local TweenService = game:GetService("TweenService")
+
+function CreateInstance(cls,props)
     local inst = Instance.new(cls)
     for i,v in pairs(props) do
         inst[i] = v
     end
     return inst
+end
+
+function CreateTween(object,time,props)
+    local tween = TweenService:Create(object,TweenInfo.new(time),{props}):Play()
+
+    return tween
 end
 
 function CreateCorner(parent,size1,size2)
@@ -15,9 +23,9 @@ end
 
 function library:CreatePopup(parent,text)
     local ScreenGui = parent
-    local Popup = CreateInstance('Frame',{Style=Enum.FrameStyle.Custom,Active=false,AnchorPoint=Vector2.new(0, 0),BackgroundColor3=Color3.new(0.168627, 0.168627, 0.168627),BackgroundTransparency=0,BorderColor3=Color3.new(0.105882, 0.164706, 0.207843),BorderSizePixel=1,ClipsDescendants=false,Draggable=false,Position=UDim2.new(0.41220656, 0, 0.431466013, 0),Rotation=0,Selectable=false,Size=UDim2.new(0.1, 0, 0.125, 0),SizeConstraint=Enum.SizeConstraint.RelativeXY,Visible=true,ZIndex=1,Name = 'Popup',Parent = ScreenGui})
+    local Popup = CreateInstance('Frame',{Style=Enum.FrameStyle.Custom,Active=false,AnchorPoint=Vector2.new(0, 0),BackgroundColor3=Color3.new(0.168627, 0.168627, 0.168627),BackgroundTransparency=0,BorderColor3=Color3.new(0.105882, 0.164706, 0.207843),BorderSizePixel=1,ClipsDescendants=false,Draggable=false,Position=UDim2.new(0.41220656, 0, 0.431466013, 0),Rotation=0,Selectable=false,Size=UDim2.new(0.1, 0, 0.1, 0),SizeConstraint=Enum.SizeConstraint.RelativeXY,Visible=true,ZIndex=1,Name = 'Popup',Parent = ScreenGui})
     local TopBar = CreateInstance('Frame',{Style=Enum.FrameStyle.Custom,Active=false,AnchorPoint=Vector2.new(0, 0),BackgroundColor3=Color3.new(0.113725, 0.113725, 0.113725),BackgroundTransparency=0,BorderColor3=Color3.new(0.105882, 0.164706, 0.207843),BorderSizePixel=1,ClipsDescendants=false,Draggable=false,Position=UDim2.new(0, 0, 0, 0),Rotation=0,Selectable=false,Size=UDim2.new(1, 0, 0.185000002, 0),SizeConstraint=Enum.SizeConstraint.RelativeXY,Visible=true,ZIndex=1,Name = 'TopBar',Parent = Popup})
-    local Frame = CreateInstance('Frame',{Style=Enum.FrameStyle.Custom,Active=false,AnchorPoint=Vector2.new(0, 0),BackgroundColor3=Color3.new(0.223529, 0.223529, 0.223529),BackgroundTransparency=0,BorderColor3=Color3.new(0.105882, 0.164706, 0.207843),BorderSizePixel=1,ClipsDescendants=false,Draggable=false,Position=UDim2.new(0.874580801, 0, 0, 0),Rotation=0,Selectable=false,Size=UDim2.new(0.125, 0, 1, 0),SizeConstraint=Enum.SizeConstraint.RelativeXY,Visible=true,ZIndex=1,Name = 'Frame',Parent = TopBar})
+    local Frame = CreateInstance('Frame',{Style=Enum.FrameStyle.Custom,Active=false,AnchorPoint=Vector2.new(0, 0),BackgroundColor3=Color3.new(0.223529, 0.223529, 0.223529),BackgroundTransparency=0,BorderColor3=Color3.new(0.105882, 0.164706, 0.207843),BorderSizePixel=1,ClipsDescendants=false,Draggable=false,Position=UDim2.new(0.874580801, 0, 0, 0),Rotation=0,Selectable=false,Size=UDim2.new(0.1, 0, 1, 0),SizeConstraint=Enum.SizeConstraint.RelativeXY,Visible=true,ZIndex=1,Name = 'Frame',Parent = TopBar})
     local TextButton = CreateInstance('TextButton',{Font=Enum.Font.SourceSans,FontSize=Enum.FontSize.Size14,Text='',TextColor3=Color3.new(1, 1, 1),TextScaled=false,TextSize=14,TextStrokeColor3=Color3.new(0, 0, 0),TextStrokeTransparency=1,TextTransparency=0,TextWrapped=true,TextXAlignment=Enum.TextXAlignment.Center,TextYAlignment=Enum.TextYAlignment.Center,AutoButtonColor=true,Modal=false,Selected=false,Style=Enum.ButtonStyle.Custom,Active=true,AnchorPoint=Vector2.new(0, 0),BackgroundColor3=Color3.new(1, 1, 1),BackgroundTransparency=1,BorderColor3=Color3.new(0.105882, 0.164706, 0.207843),BorderSizePixel=1,ClipsDescendants=false,Draggable=false,Position=UDim2.new(0, 0, 0, 0),Rotation=0,Selectable=true,Size=UDim2.new(1, 0, 1, 0),SizeConstraint=Enum.SizeConstraint.RelativeXY,Visible=true,ZIndex=2,Name='TextButton',Parent = Frame})
     local MainFrame = CreateInstance('Frame',{Style=Enum.FrameStyle.Custom,Active=false,AnchorPoint=Vector2.new(0, 0),BackgroundColor3=Color3.new(1, 1, 1),BackgroundTransparency=1,BorderColor3=Color3.new(0.105882, 0.164706, 0.207843),BorderSizePixel=1,ClipsDescendants=false,Draggable=false,Position=UDim2.new(0, 0, 0.185000122, 0),Rotation=0,Selectable=false,Size=UDim2.new(0.999580801, 0, 0.814999878, 0),SizeConstraint=Enum.SizeConstraint.RelativeXY,Visible=true,ZIndex=1,Name = 'MainFrame',Parent = Popup})
     local TextLabel = CreateInstance('TextLabel',{Font=Enum.Font.SourceSansBold,FontSize=Enum.FontSize.Size14,Text=text,TextColor3=Color3.new(1, 1, 1),TextScaled=false,TextSize=14,TextStrokeColor3=Color3.new(0, 0, 0),TextStrokeTransparency=1,TextTransparency=0,TextWrapped=true,TextXAlignment=Enum.TextXAlignment.Center,TextYAlignment=Enum.TextYAlignment.Center,Active=false,AnchorPoint=Vector2.new(0, 0),BackgroundColor3=Color3.new(1, 1, 1),BackgroundTransparency=1,BorderColor3=Color3.new(0.105882, 0.164706, 0.207843),BorderSizePixel=1,ClipsDescendants=false,Draggable=false,Position=UDim2.new(0, 0, 0, 0),Rotation=0,Selectable=false,Size=UDim2.new(1, 0, 1, 0),SizeConstraint=Enum.SizeConstraint.RelativeXY,Visible=true,ZIndex=1,Name='TextLabel',Parent = MainFrame})
@@ -28,6 +36,12 @@ function library:CreatePopup(parent,text)
     CreateCorner(MainFrame,0,2)
 
     TextButton.MouseButton1Down:Connect(function()
+        local tween = CreateTween(MainFrame,0.75,{Size = UDim2.new(MainFrame.Size.X,0,0,0)})
+        tween.Completed:Wait()
+        MainFrame:Destroy()
+
+        local tween1 = CreateTween(TopBar,0.75,{Size = UDim2.new(0,0,TopBar.Size.Y,0)})
+        tween1.Completed:Wait()
         Popup:Destroy()
     end)
 end
