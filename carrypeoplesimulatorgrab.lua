e6ad05cef0bdc724dbfe39524b531c6e1a5aaf7b
shoizz <80241798+shoizz@users.noreply.github.com>
2021-08-03 15:16:28 +1000

    Update carrypeoplesimulatorgrab.lua

--- carrypeoplesimulatorgrab.lua
+++ carrypeoplesimulatorgrab.lua
@@ -6,6 +6,10 @@ local originalpos = self.Character.HumanoidRootPart.CFrame
 
 local humanoid = self.Character.Humanoid
 
+--Options
+
+local keybind = Enum.KeyCode.LeftBracket
+
 --Services
 
 local uis = game:GetService("UserInputService")
@@ -105,39 +109,45 @@ end)
 
 local tweenservice = game:GetService("TweenService")
 
-local dragging
 local dragInput
 local dragStart
 local startPos
 local dampening = 0.3
 
-local function update(input)
-	local delta = input.Position - dragStart
-	tweenservice:Create(frame,TweenInfo.new(dampening),{Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)}):Play()
+function dragify(obj)
+    local dragging = false
+
+    local function update(input)
+        local delta = input.Position - dragStart
+        tweenservice:Create(obj,TweenInfo.new(dampening),{Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)}):Play()
+    end
+    
+    obj.InputBegan:Connect(function(input)
+        if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+            dragging = true
+            dragStart = input.Position
+            startPos = obj.Position
+    
+            input.Changed:Connect(function()
+                if input.UserInputState == Enum.UserInputState.End then
+                    dragging = false
+                end
+            end)
+        end
+    end)
+    
+    obj.InputChanged:Connect(function(input)
+        if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+            dragInput = input
+        end
+    end)
+    
+    uis.InputChanged:Connect(function(input)
+        if input == dragInput and dragging then
+            update(input)
+        end
+    end)
 end
 
-frame.InputBegan:Connect(function(input)
-	if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
-		dragging = true
-		dragStart = input.Position
-		startPos = frame.Position
-
-		input.Changed:Connect(function()
-			if input.UserInputState == Enum.UserInputState.End then
-				dragging = false
-			end
-		end)
-	end
-end)
-
-frame.InputChanged:Connect(function(input)
-	if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
-		dragInput = input
-	end
-end)
-
-uis.InputChanged:Connect(function(input)
-	if input == dragInput and dragging then
-		update(input)
-	end
-end)
+dragify(frame)
+dragify(dropdown)
