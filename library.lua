4a32d167d837dabc8ec380255df49f871919b01b
shoizz <80241798+shoizz@users.noreply.github.com>
2021-08-03 12:03:45 +1000

    Update library.lua

--- library.lua
+++ library.lua
@@ -1,45 +1,94 @@
 local library = {}
 
-local TweenService = game:GetService("TweenService")
+local tweenservice = game:GetService("TweenService")
+local floor = 250
 
-function CreateInstance(cls,props)
-    local inst = Instance.new(cls)
-    for i,v in pairs(props) do
-        inst[i] = v
-    end
-    return inst
+function library:Setup(height)
+    pcall(function()
+        if workspace.CurrentCamera.CustomMap then
+            workspace.CurrentCamera.CustomMap:Destroy()
+        end
+    end)
+
+    local folder = Instance.new("Folder")
+    folder.Name = "CustomMap"
+    folder.Parent = workspace.CurrentCamera
+
+    local buildings = Instance.new("Folder")
+    buildings.Name = "Buildings"
+    buildings.Parent = folder
+
+    local objects = Instance.new("Folder")
+    objects.Name = "Objects"
+    objects.Parent = folder
+
+    makefolder("Custom Maps")
+
+    floor = height
+
+    return folder,buildings,objects,floor
 end
 
-function CreateTween(object,time,props)
-    local tween = TweenService:Create(object,TweenInfo.new(time),props):Play()
-    tween.Completed:Wait()
+function library:FetchPlayer()
+    local player = game:GetService("Players").LocalPlayer
 
-    return tween
+    return player
 end
 
-function CreateCorner(parent,size1,size2)
-    local corner = Instance.new("UICorner",parent)
-    corner.CornerRadius = UDim.new(size1,size2)
+function library:SetPlayerPosition(player,position,delay)
+    player.Character.HumanoidRootPart.CFrame = position
+    player.Character.HumanoidRootPart.Anchored = true
+    wait(delay)
+    player.Character.HumanoidRootPart.Anchored = false
 end
 
-function library:CreatePopup(parent,text)
-    local ScreenGui = parent
-    local Popup = CreateInstance('Frame',{Style=Enum.FrameStyle.Custom,Active=false,AnchorPoint=Vector2.new(0, 0),BackgroundColor3=Color3.new(0.168627, 0.168627, 0.168627),BackgroundTransparency=0,BorderColor3=Color3.new(0.105882, 0.164706, 0.207843),BorderSizePixel=1,ClipsDescendants=false,Draggable=false,Position=UDim2.new(0.41220656, 0, 0.431466013, 0),Rotation=0,Selectable=false,Size=UDim2.new(0.1, 0, 0.1, 0),SizeConstraint=Enum.SizeConstraint.RelativeXY,Visible=true,ZIndex=1,Name = 'Popup',Parent = ScreenGui})
-    local TopBar = CreateInstance('Frame',{Style=Enum.FrameStyle.Custom,Active=false,AnchorPoint=Vector2.new(0, 0),BackgroundColor3=Color3.new(0.113725, 0.113725, 0.113725),BackgroundTransparency=0,BorderColor3=Color3.new(0.105882, 0.164706, 0.207843),BorderSizePixel=1,ClipsDescendants=false,Draggable=false,Position=UDim2.new(0, 0, 0, 0),Rotation=0,Selectable=false,Size=UDim2.new(1, 0, 0.185000002, 0),SizeConstraint=Enum.SizeConstraint.RelativeXY,Visible=true,ZIndex=1,Name = 'TopBar',Parent = Popup})
-    local Frame = CreateInstance('Frame',{Style=Enum.FrameStyle.Custom,Active=false,AnchorPoint=Vector2.new(0, 0),BackgroundColor3=Color3.new(0.223529, 0.223529, 0.223529),BackgroundTransparency=0,BorderColor3=Color3.new(0.105882, 0.164706, 0.207843),BorderSizePixel=1,ClipsDescendants=false,Draggable=false,Position=UDim2.new(0.874580801, 0, 0, 0),Rotation=0,Selectable=false,Size=UDim2.new(0.1, 0, 1, 0),SizeConstraint=Enum.SizeConstraint.RelativeXY,Visible=true,ZIndex=1,Name = 'Frame',Parent = TopBar})
-    local TextButton = CreateInstance('TextButton',{Font=Enum.Font.SourceSans,FontSize=Enum.FontSize.Size14,Text='',TextColor3=Color3.new(1, 1, 1),TextScaled=false,TextSize=14,TextStrokeColor3=Color3.new(0, 0, 0),TextStrokeTransparency=1,TextTransparency=0,TextWrapped=true,TextXAlignment=Enum.TextXAlignment.Center,TextYAlignment=Enum.TextYAlignment.Center,AutoButtonColor=true,Modal=false,Selected=false,Style=Enum.ButtonStyle.Custom,Active=true,AnchorPoint=Vector2.new(0, 0),BackgroundColor3=Color3.new(1, 1, 1),BackgroundTransparency=1,BorderColor3=Color3.new(0.105882, 0.164706, 0.207843),BorderSizePixel=1,ClipsDescendants=false,Draggable=false,Position=UDim2.new(0, 0, 0, 0),Rotation=0,Selectable=true,Size=UDim2.new(1, 0, 1, 0),SizeConstraint=Enum.SizeConstraint.RelativeXY,Visible=true,ZIndex=2,Name='TextButton',Parent = Frame})
-    local MainFrame = CreateInstance('Frame',{Style=Enum.FrameStyle.Custom,Active=false,AnchorPoint=Vector2.new(0, 0),BackgroundColor3=Color3.new(1, 1, 1),BackgroundTransparency=1,BorderColor3=Color3.new(0.105882, 0.164706, 0.207843),BorderSizePixel=1,ClipsDescendants=false,Draggable=false,Position=UDim2.new(0, 0, 0.185000122, 0),Rotation=0,Selectable=false,Size=UDim2.new(0.999580801, 0, 0.814999878, 0),SizeConstraint=Enum.SizeConstraint.RelativeXY,Visible=true,ZIndex=1,Name = 'MainFrame',Parent = Popup})
-    local TextLabel = CreateInstance('TextLabel',{Font=Enum.Font.SourceSansBold,FontSize=Enum.FontSize.Size14,Text=text,TextColor3=Color3.new(1, 1, 1),TextScaled=false,TextSize=14,TextStrokeColor3=Color3.new(0, 0, 0),TextStrokeTransparency=1,TextTransparency=0,TextWrapped=true,TextXAlignment=Enum.TextXAlignment.Center,TextYAlignment=Enum.TextYAlignment.Center,Active=false,AnchorPoint=Vector2.new(0, 0),BackgroundColor3=Color3.new(1, 1, 1),BackgroundTransparency=1,BorderColor3=Color3.new(0.105882, 0.164706, 0.207843),BorderSizePixel=1,ClipsDescendants=false,Draggable=false,Position=UDim2.new(0, 0, 0, 0),Rotation=0,Selectable=false,Size=UDim2.new(1, 0, 1, 0),SizeConstraint=Enum.SizeConstraint.RelativeXY,Visible=true,ZIndex=1,Name='TextLabel',Parent = MainFrame})
+function library:Build(name,parent,placetime,buildtime,position,rotation,size,transparency,material)
+    local part = Instance.new("Part")
+    part.Name = name
+    part.Material = material
+    part.Parent = parent
+    part.Transparency = transparency
+    part.CFrame = CFrame.new(position.X,0,position.Z)
+    part.Size = Vector3.new(size.X/7.5,size.Y,size.Z/7.5)
+    part.Anchored = true
 
-    CreateCorner(Popup,0,2)
-    CreateCorner(TopBar,0,2)
-    CreateCorner(Frame,0,2)
-    CreateCorner(MainFrame,0,2)
+    local tweenb = tweenservice:Create(part, TweenInfo.new(placetime), {CFrame = position * rotation})
+    tweenb:Play()
+    tweenb.Completed:Wait()
 
-    TextButton.MouseButton1Down:Connect(function()
+    local tweena = tweenservice:Create(part, TweenInfo.new(buildtime), {Size = size})
+    tweena:Play()
+    tweena.Completed:Wait()
+end
 
-        local tween = CreateTween(Popup,0.75,{Size = UDim2.new(MainFrame.Size.X,0,0,0)})
-        Popup:Destroy()
+function library:Chair(obj,sitheight,debounce)
+    local sitdebounce = false
+    
+    obj.Touched:Connect(function(hit)
+        if hit.Parent:FindFirstChild("Humanoid") then
+            if sitdebounce == false then
+                sitdebounce = true
+    
+                hit.Parent:FindFirstChild("Humanoid").Sit = true
+                hit.Parent:FindFirstChild("HumanoidRootPart").CFrame = obj.CFrame*CFrame.new(0,sitheight,0)
+                
+                wait(debounce)
+    
+                sitdebounce = false
+            end
+        end
+    end)
+end
+
+function library:LaunchPad(obj,height)
+    obj.Touched:Connect(function(hit)
+        if hit.Parent:FindFirstChild("Humanoid") then
+            local bv = Instance.new("BodyVelocity",hit.Parent:FindFirstChild("HumanoidRootPart"))
+            bv.MaxForce = Vector3.new(math.huge,math.huge,math.huge)
+            bv.Velocity = Vector3.new(0,1*height,0)
+            wait(0.1)
+            bv:Destroy()
+        end
     end)
 end
 
